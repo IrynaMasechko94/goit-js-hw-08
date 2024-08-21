8aa325081c2df413d0aa6e9f00ace4c5a5bb6b25
IrynaMasechko94 <165693960+IrynaMasechko94@users.noreply.github.com>
2024-08-21 19:06:58 +0300

    added task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+const markup = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+   <a class="gallery__link" href=${item.original}>
+      <img class="gallery__image" src=${item.preview} alt=${item.description} title=${item.description} />
+   </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import { throttle } from 'lodash';
 
+const inputForm = document.querySelector('.feedback-form');
+
+const throttledSave = throttle(form => {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const data = {
+    email,
+    message,
+  };
+  console.log(data);
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
+
+inputForm.addEventListener('input', event => {
+  const form = event.target.form;
+  throttledSave(form);
+});
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+if (savedData) {
+  const { email, message } = JSON.parse(savedData);
+
+  if (inputForm) {
+    inputForm.elements.email.value = email || '';
+    inputForm.elements.message.value = message || '';
+  }
+}
+
+inputForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+}
